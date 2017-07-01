acdbb58c20583cb7a44ea5ec8d27f3c67dd08b10
jikpas0 <jikpas0@hotmail.com>
2017-07-01 13:47:51 +0100

    Configured nuget packages, setup starting point for tests and view of contact page

---
+++ QA.ContactUs.Tests/FeatureTest/ContactUsPageSteps.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace QA.ContactUs.Tests.FeatureTest
+{
+    [Binding]
+    public class ContactUsPageSteps
+    {
+        [Given(@"I am on the QAWorks Site")]
+        public void GivenIAmOnTheQAWorksSite()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should be able to contact QAWorks with the following information")]
+        public void ThenIShouldBeAbleToContactQAWorksWithTheFollowingInformation(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ QA.ContactUs.Tests/Views/ContactUsTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace QA.ContactUs.Tests.Views
+{
+    [TestFixture]
+    public class ContactUsTest
+    {
+        private IWebDriver driver = new ChromeDriver();
+
+        [SetUp]
+        public void init()
+        {
+            
+            driver.Url = "http://www.demoqa.com";
+        }
+
+        [TearDown]
+        public void Dispose()
+        {
+            driver.Close();
+        }
+
+        [Test]
+        public void Page()
+        {  }
+    }
+}
