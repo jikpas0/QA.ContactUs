39c09150197cc4132fc5cf153b0506ae71273b06
jikpas0 <jikpas0@hotmail.com>
2017-07-02 11:01:24 +0100

    Implemented test values for feature

--- QA.ContactUs.Tests/FeatureTest/ContactUsPageSteps.cs
+++ QA.ContactUs.Tests/FeatureTest/ContactUsPageSteps.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using TechTalk.SpecFlow;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow.Assist;
 
 namespace QA.ContactUs.Tests.FeatureTest
 {
@@ -23,13 +25,34 @@ namespace QA.ContactUs.Tests.FeatureTest
         [Then(@"I should be able to contact QAWorks with the following information")]
         public void ThenIShouldBeAbleToContactQAWorksWithTheFollowingInformation(Table table)
         {
+            var dictionary = TableExtensions.ToDictionary(table);
             IWebElement nameElement = driver.FindElement(By.Id("Name"));
-            // Action can be performed on Input Button element
-            nameElement.SendKeys("Hello World");
-            Thread.Sleep(5000);
+            nameElement.SendKeys(dictionary["name"]);
+            Thread.Sleep(1000);
+
+            IWebElement emailElement = driver.FindElement(By.Id("Email"));
+            emailElement.SendKeys(dictionary["email"]);
+            Thread.Sleep(1000);
+
+            IWebElement messageElement = driver.FindElement(By.Id("Message"));
+            messageElement.SendKeys(dictionary["message"]);
+            Thread.Sleep(1000);
+
             IWebElement submitElement = driver.FindElement(By.Name("Send"));
-            // Action can be performed on Input Button element
             submitElement.Submit();
+        }
+    }
+
+    public class TableExtensions
+    {
+        public static Dictionary<string, string> ToDictionary(Table table)
+        {
+            var dictionary = new Dictionary<string, string>();
+            foreach (var row in table.Rows)
+            {
+                dictionary.Add(row[0], row[1]);
+            }
+            return dictionary;
         }
     }
 }
